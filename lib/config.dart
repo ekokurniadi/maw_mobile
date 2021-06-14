c73f8c78eea08ce8515241829bb49c51b18e150b
Eko Kurniadi <ekokurniadi.02@gmail.com>
2021-06-14 10:57:47 +0700

    update link api

--- lib/config.dart
+++ lib/config.dart
@@ -1,9 +1,6 @@
 class Config {
-  static const BASE_URL = "http://192.168.43.146/multiandalan/api/";
-  static const BASE_URL_IMAGE = "http://192.168.43.146/multiandalan/";
-  static const BASE_URL_WEB = "http://192.168.43.146/multiandalan/";
-  // static const BASE_URL = "http://jual-beliooo.000webhostapp.com/api/";
-  // static const BASE_URL_IMAGE = "https://jual-beliooo.000webhostapp.com/";
-
+  static const BASE_URL = "http://192.168.43.146/multiandalanwisesa/api/";
+  static const BASE_URL_IMAGE = "http://192.168.43.146/multiandalanwisesa/";
+  static const BASE_URL_WEB = "http://192.168.43.146/multiandalanwisesa/";
   static const API_KEY = "AIzaSyAAyfmnFvhRQqjFSW7euy935Pm8gVq9GE0";
 }
