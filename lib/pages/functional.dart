8beb8ce0bb9e4d7c1cd91b0cffd22ab518498507
ekokurniadi <ekokurniadi.02@gmail.com>
2021-06-19 17:03:20 +0700

    menambahkan fungsi save absen

--- lib/pages/functional.dart
+++ lib/pages/functional.dart
@@ -5,6 +5,7 @@ import '../helper.dart';
 import 'dart:convert';
 import 'package:geocoder/geocoder.dart';
 import 'package:geolocator/geolocator.dart';
+
 final Helper helper = Helper();
 DateTime _dateTime = DateTime.now();
 
@@ -27,14 +28,26 @@ class Functional {
       "latitude": position.latitude.toString(),
       "longitude": position.longitude.toString(),
       "currentloc": alamat.addressLine.toString(),
-      "update_lokasi_at":"$_dateTime"
+      "update_lokasi_at": _dateTime.toString()
     });
     final res = jsonDecode(response.body);
     // print(res);
     if (res['status'] == 200) {
       // print("Lokasi Petugas saat ini : $position");
+      getDistance(position.latitude, position.latitude);
     } else {
       print("Gagal mendapatkan lokasi");
     }
+  }
+
+  getDistance(double userLat, double userLong) async {
+    final response = await http.post(Config.BASE_URL + "getLokasiKantor");
+    final res = jsonDecode(response.body);
+    double officeLat = double.parse(res['latitude']);
+    double officeLong = double.parse(res['longitude']);
+    print(res);
+    Future<double> distanceInMeters =
+        Geolocator().distanceBetween(userLat, officeLat, userLong, officeLong);
+    return distanceInMeters;
   }
 }
--- lib/pages/sales/absen.dart
+++ lib/pages/sales/absen.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'dart:io';
@@ -10,6 +9,7 @@ import 'package:maw/pages/functional.dart';
 import '../../config.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:intl/intl.dart';
+import 'package:geolocator/geolocator.dart';
 
 class Absen extends StatefulWidget {
   final String title = "Upload Image Demo";
@@ -26,6 +26,10 @@ class AbsenState extends State<Absen> {
   File tmpFile;
   String errMessage = 'Error Uploading Image';
   DateTime dateTime = DateTime.now();
+  double officeLat;
+  double officeLong;
+  double distance;
+  String jarakOnDevice = "";
   chooseImage() {
     setState(() {
       // ignore: deprecated_member_use
@@ -58,19 +62,48 @@ class AbsenState extends State<Absen> {
     upload(fileName);
   }
 
+  sendLocation() async {
+    Position position = await Geolocator()
+        .getCurrentPosition(desiredAccuracy: LocationAccuracy.best);
+    final response = await http.post(Config.BASE_URL + "getLokasiKantor");
+    final res = jsonDecode(response.body);
+    setState(() {
+      officeLat = double.parse(res['latitude']);
+      officeLong = double.parse(res['latitude']);
+    });
+    print(res);
+    double distanceInMeters = await Geolocator().distanceBetween(
+        -6.59738178531859,
+        106.76834551261724,
+        -6.5973977720028545,
+        106.76830729113959);
+    // double distanceInMeters = await Geolocator().distanceBetween(
+    //     position.latitude, position.longitude, officeLat, officeLong);
+    setState(() {
+      distance = distanceInMeters;
+      jarakOnDevice = distanceInMeters.toStringAsFixed(2);
+    });
+
+    // helper.alertLog(distance.toStringAsFixed(2) + " Meter");
+  }
+
   upload(String fileName) async {
     SharedPreferences pref = await SharedPreferences.getInstance();
     var idUser = pref.getString("id");
-    String linkToServer = "absenSales";
+    String linkToServer = "absensi";
 
     http.post(Config.BASE_URL + linkToServer, body: {
       "image": base64Image,
       "name": fileName,
-      "id": idUser
+      "id_sales": idUser,
+      "tanggal": dateTime.toString(),
+      "jam": dateTime.toString(),
+      "jarak": distance.toStringAsFixed(2),
     }).then((result) {
       final data = jsonDecode(result.body);
+      print(data);
       setStatus(result.statusCode == 200 ? data['pesan'] : errMessage);
-      if (data['status'] == "1") {
+      if (data['status'] == 200) {
         helper.alertSuccess(data['pesan'], context);
       } else {
         helper.alertSuccess(data['pesan'], context);
@@ -88,10 +121,13 @@ class AbsenState extends State<Absen> {
             null != snapshot.data) {
           tmpFile = snapshot.data;
           base64Image = base64Encode(snapshot.data.readAsBytesSync());
-          return Flexible(
-            child: Image.file(
-              snapshot.data,
-              fit: BoxFit.cover,
+          return GestureDetector(
+            onTap: chooseImages,
+            child: Flexible(
+              child: Image.file(
+                snapshot.data,
+                fit: BoxFit.contain,
+              ),
             ),
           );
         } else if (null != snapshot.error) {
@@ -100,12 +136,15 @@ class AbsenState extends State<Absen> {
             textAlign: TextAlign.center,
           );
         } else {
-          return Container(
-            width: MediaQuery.of(context).size.width,
-            height: MediaQuery.of(context).size.height * 0.40,
-            child: Image.asset(
-              "images/no-image.png",
-              fit: BoxFit.contain,
+          return GestureDetector(
+            onTap: chooseImages,
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height * 0.40,
+              child: Image.asset(
+                "images/no-image.png",
+                fit: BoxFit.contain,
+              ),
             ),
           );
         }
@@ -117,7 +156,9 @@ class AbsenState extends State<Absen> {
   @override
   void initState() {
     super.initState();
+    sendLocation();
     timer = Timer.periodic(Duration(seconds: 1), (Timer t) {
+      sendLocation();
       setState(() {
         dateTime = DateTime.now();
       });
@@ -135,72 +176,82 @@ class AbsenState extends State<Absen> {
     return Scaffold(
       body: Container(
         padding: EdgeInsets.all(30.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            showImage(),
-            GestureDetector(
-              onTap: chooseImages,
-              child: Container(
-                  color: Colors.red,
-                  margin:
-                      EdgeInsets.symmetric(horizontal: 34.0, vertical: 10.0),
-                  width: MediaQuery.of(context).size.width,
-                  height: 50,
-                  child: Center(
-                      child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Icon(Icons.camera, color: Colors.white),
-                      Text(
-                        'Ambil Foto Selfie',
-                        style: GoogleFonts.poppins(
-                            color: Colors.white, fontWeight: FontWeight.w400),
-                      ),
-                    ],
-                  ))),
-            ),
-            SizedBox(
-              height: 20.0,
-            ),
-            Container(
-                child: Center(
-                    child: Text(DateFormat('dd/MM/yyyy H:mm:ss')
-                        .format(dateTime)
-                        .toString()))),
-            SizedBox(
-              height: 20.0,
-            ),
-            GestureDetector(
-              onTap: startUpload,
-              child: Container(
-                  color: Colors.blue,
-                  margin: EdgeInsets.symmetric(horizontal: 34.0),
-                  width: MediaQuery.of(context).size.width,
-                  height: 50,
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  "Jarak lokasi anda ke kantor : " + jarakOnDevice + " M",
+                  style: GoogleFonts.poppins(),
+                ),
+              ),
+              showImage(),
+              // GestureDetector(
+              //   onTap: chooseImages,
+              //   child: Container(
+              //       color: Colors.red,
+              //       margin:
+              //           EdgeInsets.symmetric(horizontal: 34.0, vertical: 10.0),
+              //       width: MediaQuery.of(context).size.width,
+              //       height: 50,
+              //       child: Center(
+              //           child: Row(
+              //         mainAxisAlignment: MainAxisAlignment.center,
+              //         children: [
+              //           Icon(Icons.camera, color: Colors.white),
+              //           Text(
+              //             'Ambil Foto Selfie',
+              //             style: GoogleFonts.poppins(
+              //                 color: Colors.white, fontWeight: FontWeight.w400),
+              //           ),
+              //         ],
+              //       ))),
+              // ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Container(
                   child: Center(
                       child: Text(
-                    'Absen Sekarang',
-                    style: GoogleFonts.poppins(
-                        color: Colors.white, fontWeight: FontWeight.w400),
-                  ))),
-            ),
-            SizedBox(
-              height: 20.0,
-            ),
-            Text(
-              status,
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                color: Colors.green,
-                fontWeight: FontWeight.w500,
-                fontSize: 20.0,
+                DateFormat('dd/MM/yyyy H:mm:ss').format(dateTime).toString(),
+                style: GoogleFonts.poppins(color: Colors.blueGrey),
+              ))),
+              SizedBox(
+                height: 20.0,
               ),
-            ),
-            SizedBox(
-              height: 20.0,
-            ),
-          ],
+              GestureDetector(
+                onTap: base64Image == null ? null : startUpload,
+                child: Container(
+                    color: base64Image == null ? Colors.blueGrey : Colors.red,
+                    margin: EdgeInsets.symmetric(horizontal: 34.0),
+                    width: MediaQuery.of(context).size.width,
+                    height: 50,
+                    child: Center(
+                        child: Text(
+                      'Absen Sekarang',
+                      style: GoogleFonts.poppins(
+                          color: Colors.white, fontWeight: FontWeight.w400),
+                    ))),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Text(
+                status,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.green,
+                  fontWeight: FontWeight.w500,
+                  fontSize: 20.0,
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+            ],
+          ),
         ),
       ),
     );
